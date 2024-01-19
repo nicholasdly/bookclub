9377ba656b69563c81c332f7d2b0d026e0d6bd19
Nicholas Ly <nichdly@gmail.com>
2024-01-19 03:51:35 -0600

    applied prettier formatting

--- src/app/_components/icons.tsx
+++ src/app/_components/icons.tsx
@@ -4,17 +4,43 @@ interface IconProps {
 
 export function BookmarkFilledIcon({ className }: IconProps) {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" className={className} width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-      <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-      <path d="M14 2a5 5 0 0 1 5 5v14a1 1 0 0 1 -1.555 .832l-5.445 -3.63l-5.444 3.63a1 1 0 0 1 -1.55 -.72l-.006 -.112v-14a5 5 0 0 1 5 -5h4z" stroke-width="0" fill="currentColor" />
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+      width="44"
+      height="44"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      fill="none"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path
+        d="M14 2a5 5 0 0 1 5 5v14a1 1 0 0 1 -1.555 .832l-5.445 -3.63l-5.444 3.63a1 1 0 0 1 -1.55 -.72l-.006 -.112v-14a5 5 0 0 1 5 -5h4z"
+        stroke-width="0"
+        fill="currentColor"
+      />
     </svg>
-    );
+  );
 }
 
 export function BookIcon({ className }: IconProps) {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" className={className} width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-      <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+      width="44"
+      height="44"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      fill="none"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
       <path d="M3 19a9 9 0 0 1 9 0a9 9 0 0 1 9 0" />
       <path d="M3 6a9 9 0 0 1 9 0a9 9 0 0 1 9 0" />
       <path d="M3 6l0 13" />
@@ -26,8 +52,19 @@ export function BookIcon({ className }: IconProps) {
 
 export function PencilIcon({ className }: IconProps) {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" className={className} width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-      <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+      width="44"
+      height="44"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      fill="none"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
       <path d="M4 20h4l10.5 -10.5a2.828 2.828 0 1 0 -4 -4l-10.5 10.5v4" />
       <path d="M13.5 6.5l4 4" />
     </svg>
@@ -36,8 +73,19 @@ export function PencilIcon({ className }: IconProps) {
 
 export function MessageOffIcon({ className }: IconProps) {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" className={className} width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-      <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+      width="44"
+      height="44"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      fill="none"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
       <path d="M8 9h1m4 0h3" />
       <path d="M8 13h5" />
       <path d="M8 4h10a3 3 0 0 1 3 3v8c0 .577 -.163 1.116 -.445 1.573m-2.555 1.427h-5l-5 3v-3h-2a3 3 0 0 1 -3 -3v-8c0 -1.085 .576 -2.036 1.439 -2.562" />
@@ -48,8 +96,19 @@ export function MessageOffIcon({ className }: IconProps) {
 
 export function MoodSmileBeamIcon({ className }: IconProps) {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" className={className} width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-      <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+      width="44"
+      height="44"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      fill="none"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
       <path d="M12 21a9 9 0 1 1 0 -18a9 9 0 0 1 0 18z" />
       <path d="M10 10c-.5 -1 -2.5 -1 -3 0" />
       <path d="M17 10c-.5 -1 -2.5 -1 -3 0" />
@@ -60,8 +119,19 @@ export function MoodSmileBeamIcon({ className }: IconProps) {
 
 export function BooksIcon({ className }: IconProps) {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" className={className} width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-      <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+      width="44"
+      height="44"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      fill="none"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
       <path d="M5 4m0 1a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v14a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1z" />
       <path d="M9 4m0 1a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v14a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1z" />
       <path d="M5 8h4" />
@@ -75,8 +145,19 @@ export function BooksIcon({ className }: IconProps) {
 
 export function UserCircleIcon({ className }: IconProps) {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" className={className} width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-      <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+      width="44"
+      height="44"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      fill="none"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
       <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
       <path d="M12 10m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
       <path d="M6.168 18.849a4 4 0 0 1 3.832 -2.849h4a4 4 0 0 1 3.834 2.855" />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
 import Link from "next/link";
-import { BookIcon, BookmarkFilledIcon, BooksIcon, MessageOffIcon, MoodSmileBeamIcon, PencilIcon, UserCircleIcon } from "./_components/icons";
+import {
+  BookIcon,
+  BookmarkFilledIcon,
+  BooksIcon,
+  MessageOffIcon,
+  MoodSmileBeamIcon,
+  PencilIcon,
+  UserCircleIcon,
+} from "./_components/icons";
 import { Button } from "./_components/shadcn-ui/button";
 
 export default function Home() {
@@ -19,7 +27,7 @@ export default function Home() {
 function Header() {
   return (
     <header>
-      <nav className="mx-auto flex max-w-7xl items-center justify-between py-8 px-4 sm:px-6 lg:px-8">
+      <nav className="mx-auto flex max-w-7xl items-center justify-between px-4 py-8 sm:px-6 lg:px-8">
         <div className="flex items-center gap-16">
           <Link href="/" className="flex items-center gap-2">
             <BookmarkFilledIcon className="h-10 w-auto text-stone-500" />
@@ -34,7 +42,9 @@ function Header() {
             </Button>
           </div>
         </div>
-        <Button variant="outline" className="pointer-events-none">Coming soon</Button>
+        <Button variant="outline" className="pointer-events-none">
+          Coming soon
+        </Button>
       </nav>
     </header>
   );
@@ -42,20 +52,23 @@ function Header() {
 
 function Hero() {
   return (
-    <div className="py-24 sm:py-36 pb-32 sm:pb-40 mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 lg:grid lg:grid-cols-12">
+    <div className="mx-auto max-w-7xl px-4 py-24 pb-32 sm:px-6 sm:py-36 sm:pb-40 lg:grid lg:grid-cols-12 lg:px-8">
       <div className="max-w-2xl lg:col-span-6 lg:max-w-none">
         <h1 className="text-4xl font-medium tracking-tight">
           The social platform for bookworms.
         </h1>
         <p className="mt-6 text-lg text-stone-600">
-          <span className="font-bold">tldr</span> is a place to read, review, and talk about books with the
-          internet. Keep track of the books you&apos;ve read, interact with other readers, and build your bookworm
-          community—all in one place. 
+          <span className="font-bold">tldr</span> is a place to read, review,
+          and talk about books with the internet. Keep track of the books
+          you&apos;ve read, interact with other readers, and build your bookworm
+          community—all in one place.
         </p>
-        <Button variant="outline" className="pointer-events-none mt-6">Coming soon</Button>
+        <Button variant="outline" className="pointer-events-none mt-6">
+          Coming soon
+        </Button>
       </div>
     </div>
-  )
+  );
 }
 
 function Features() {
@@ -63,56 +76,66 @@ function Features() {
     {
       icon: BookIcon,
       title: "Track your reading",
-      description: "Track every single book you're reading, have read, or plan to read in an easy to use reading log with insightful stats.",
+      description:
+        "Track every single book you're reading, have read, or plan to read in an easy to use reading log with insightful stats.",
     },
     {
       icon: PencilIcon,
       title: "Post what you want",
-      description: "Whether its reviews, rants, or anything in between: share what you want, whenever you want, and interact with others in real time.",
+      description:
+        "Whether its reviews, rants, or anything in between: share what you want, whenever you want, and interact with others in real time.",
     },
     {
       icon: MessageOffIcon,
       title: "280 character limit",
-      description: "All posts—including reviews—are all character limited. We're all about keeping things short and sweet here.",
+      description:
+        "All posts—including reviews—are all character limited. We're all about keeping things short and sweet here.",
     },
     {
       icon: MoodSmileBeamIcon,
       title: "Your own personal feed",
-      description: "Stay up to date with all the people, books, and topics you decide to follow.",
+      description:
+        "Stay up to date with all the people, books, and topics you decide to follow.",
     },
     {
       icon: BooksIcon,
       title: "Link books to your posts",
-      description: "Create posts that reference a book and page, so your followers know exactly what you're talking about.",
+      description:
+        "Create posts that reference a book and page, so your followers know exactly what you're talking about.",
     },
     {
       icon: UserCircleIcon,
       title: "Personalize your profile",
-      description: "Customize aspects of your profile to make it as personal (or generic) as you like.",
+      description:
+        "Customize aspects of your profile to make it as personal (or generic) as you like.",
     },
   ];
 
   return (
-    <section id="features" className="bg-stone-900 py-20 sm:py-32 pb-24 sm:pb-36 px-4 sm:px-6 lg:px-8">
+    <section
+      id="features"
+      className="bg-stone-900 px-4 py-20 pb-24 sm:px-6 sm:py-32 sm:pb-36 lg:px-8"
+    >
       <div className="mx-auto max-w-7xl">
         <div className="max-w-2xl lg:max-w-3xl">
           <h2 className="text-3xl font-medium tracking-tight text-stone-50">
             All the features you want, with more to come.
           </h2>
           <p className="mt-2 text-lg text-stone-400">
-            <span className="font-bold">tldr</span> was built for readers, and pulls inspiration from the best parts
-            of other social media platforms such as Goodreads and Twitter to provide a performant and intuitive user
-            experience.
+            <span className="font-bold">tldr</span> was built for readers, and
+            pulls inspiration from the best parts of other social media
+            platforms such as Goodreads and Twitter to provide a performant and
+            intuitive user experience.
           </p>
         </div>
-        <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 mt-10">
+        <ul className="mt-10 grid grid-cols-1 gap-10 sm:grid-cols-2 lg:grid-cols-3">
           {features.map((feature, index) => (
             <li key={index}>
-              <div className="flex items-center gap-3 mb-3">
-                <feature.icon className="text-stone-300 w-auto h-6" />
-                <h3 className="text-stone-300 text-lg">{feature.title}</h3>
+              <div className="mb-3 flex items-center gap-3">
+                <feature.icon className="h-6 w-auto text-stone-300" />
+                <h3 className="text-lg text-stone-300">{feature.title}</h3>
               </div>
-              <p className="text-stone-400 px-3">{feature.description}</p>
+              <p className="px-3 text-stone-400">{feature.description}</p>
             </li>
           ))}
         </ul>
@@ -125,46 +148,64 @@ function FAQs() {
   const faqs = [
     {
       question: "Is tldr free?",
-      answer: "Yes! tldr in its entirety will be completely free to use, although donations will go a long way to support the team.",
+      answer:
+        "Yes! tldr in its entirety will be completely free to use, although donations will go a long way to support the team.",
     },
     {
       question: "How is tldr any different from Goodreads?",
-      answer: "tldr's primary feature is its Twitter-like personal feed and social interactions, on top of having a beautifully intuitive book tracker. tldr has no affiliation with Amazon or any other corporation.",
+      answer:
+        "tldr's primary feature is its Twitter-like personal feed and social interactions, on top of having a beautifully intuitive book tracker. tldr has no affiliation with Amazon or any other corporation.",
     },
     {
       question: "When can I use tldr?",
-      answer: "tldr is currently being worked on as a passion project by a one person team that already work full time, so it will be a long time before a full release. Expect beta (or even alpha) releases though!",
+      answer:
+        "tldr is currently being worked on as a passion project by a one person team that already work full time, so it will be a long time before a full release. Expect beta (or even alpha) releases though!",
     },
     {
       question: "What is planned in the future?",
-      answer: "An equally as intuitive mobile app is something the team would really like to look into in the future.",
+      answer:
+        "An equally as intuitive mobile app is something the team would really like to look into in the future.",
     },
     {
       question: "Who's behind tldr?",
-      answer: "Nicholas Ly is one and only web developer working on tldr at the moment as a passion project on the side of his full time job.",
+      answer:
+        "Nicholas Ly is one and only web developer working on tldr at the moment as a passion project on the side of his full time job.",
     },
     {
       question: "Can I help work on tldr?",
-      answer: "Currently, tldr is a private project primarily for the learning experience. As the core of tldr gets flushed out though, you can expect the website to be open sourced and open to contribution.",
+      answer:
+        "Currently, tldr is a private project primarily for the learning experience. As the core of tldr gets flushed out though, you can expect the website to be open sourced and open to contribution.",
     },
   ];
 
   return (
-    <section id="faqs" className="py-20 sm:py-32 pb-24 sm:pb-36 px-4 sm:px-6 lg:px-8">
+    <section
+      id="faqs"
+      className="px-4 py-20 pb-24 sm:px-6 sm:py-32 sm:pb-36 lg:px-8"
+    >
       <div className="mx-auto max-w-7xl">
         <div className="max-w-2xl lg:max-w-3xl">
           <h2 className="text-3xl font-medium tracking-tight">
             Frequently asked questions
           </h2>
           <p className="mt-2 text-lg text-stone-600">
-            If you have anything else you would like to ask, <Link href="mailto:nichdly@gmail.com" className="underline text-stone-800">reach out to us</Link>.
+            If you have anything else you would like to ask,{" "}
+            <Link
+              href="mailto:nichdly@gmail.com"
+              className="text-stone-800 underline"
+            >
+              reach out to us
+            </Link>
+            .
           </p>
         </div>
-        <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 mt-10">
+        <ul className="mt-10 grid grid-cols-1 gap-10 sm:grid-cols-2 lg:grid-cols-3">
           {faqs.map(({ question, answer }, index) => (
             <li key={index}>
-              <h3 className="text-stone-800 text-lg mb-3 font-medium">{question}</h3>
-              <p className="text-stone-600 px-3">{answer}</p>
+              <h3 className="mb-3 text-lg font-medium text-stone-800">
+                {question}
+              </h3>
+              <p className="px-3 text-stone-600">{answer}</p>
             </li>
           ))}
         </ul>
@@ -175,7 +216,7 @@ function FAQs() {
 
 function Footer() {
   return (
-    <footer className="border-t border-stone-200 px-4 sm:px-6 lg:px-8 mb-8">
+    <footer className="mb-8 border-t border-stone-200 px-4 sm:px-6 lg:px-8">
       <div className="mx-auto max-w-7xl">
         <div className="flex justify-center pb-6 pt-16 lg:py-16">
           <div className="flex flex-col items-center">
@@ -183,7 +224,9 @@ function Footer() {
               <BookmarkFilledIcon className="h-12 w-auto text-stone-500" />
               <div>
                 <p className="text-lg font-semibold">tldr</p>
-                <p className="mt-0.5 text-sm">The social platform for bookworms.</p>
+                <p className="mt-0.5 text-sm">
+                  The social platform for bookworms.
+                </p>
               </div>
             </div>
             <nav className="mt-11 flex gap-8">
@@ -196,7 +239,7 @@ function Footer() {
             </nav>
           </div>
         </div>
-        <div className="flex justify-center text-stone-500 text-sm border-t border-stone-200">
+        <div className="flex justify-center border-t border-stone-200 text-sm text-stone-500">
           <p className="mt-6">
             &copy; Copyright {new Date().getFullYear()}. All rights reserved.
           </p>
