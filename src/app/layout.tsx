6e3a09add844957115ac8128735a0f0a3473c11e
Nicholas Ly <nichdly@gmail.com>
2024-01-28 03:42:51 -0600

    Added new shadcn/ui components + npm packages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import { cookies } from "next/headers";
 import { TRPCReactProvider } from "~/trpc/react";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "./_components/shadcn-ui/toaster";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -27,6 +28,7 @@ export default function RootLayout({
         <body className={`font-sans ${inter.variable}`}>
           <TRPCReactProvider cookies={cookies().toString()}>
             {children}
+            <Toaster />
           </TRPCReactProvider>
         </body>
       </html>
