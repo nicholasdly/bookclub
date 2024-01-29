56cf2d537758aa7b934f783fb8df12235adf8c7c
Nicholas Ly <nichdly@gmail.com>
2024-01-29 17:54:16 -0600

    Prettier formatting

--- src/app/api/antisleep/route.ts
+++ src/app/api/antisleep/route.ts
@@ -1,10 +1,10 @@
-import { NextResponse } from 'next/server';
-import { api } from '~/trpc/server';
+import { NextResponse } from "next/server";
+import { api } from "~/trpc/server";
 
 export async function GET() {
   await api.posts.antisleep.mutate();
   return NextResponse.json(
     { body: "Successfully completed antisleep routine!" },
-    { status: 200 }
+    { status: 200 },
   );
 }
