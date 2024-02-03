d65d807e455ca7298ab4ff212ae012908a113b43
Nicholas Ly <nichdly@gmail.com>
2024-02-03 15:26:07 -0600

    Clerk middleware temporary fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,9 @@
 import { authMiddleware } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
-// This example protects all routes including api/trpc routes
-// Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-  publicRoutes: (req) => !req.url.includes("/home"),
+  publicRoutes: ["/", "/api/webhooks(.*)"],
   afterAuth(auth, req) {
     // Handle users who aren't authenticated
     if (!auth.userId && !auth.isPublicRoute) {
