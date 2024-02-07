c6bedbf0577f4a14d58e2913ba956ada0a628f33
Nicholas Ly <nichdly@gmail.com>
2024-02-07 09:37:51 -0600

    Added new icon, and dialog and dropdown shadcn/ui components

--- src/app/_components/icons.tsx
+++ src/app/_components/icons.tsx
@@ -283,6 +283,15 @@ export function AuthorIcon({ className }: IconProps) {
   );
 }
 
+export function DotsIcon({ className }: IconProps) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" className={className} width="32" height="32" fill="currentColor" viewBox="0 0 256 256">
+      <path d="M144,128a16,16,0,1,1-16-16A16,16,0,0,1,144,128ZM60,112a16,16,0,1,0,16,16A16,16,0,0,0,60,112Zm136,0a16,16,0,1,0,16,16A16,16,0,0,0,196,112Z" />
+    </svg>
+  );
+}
+
+
 // export function Icon({ className }: IconProps) {
 //   return (
 
