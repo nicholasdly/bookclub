ddb715cd306fe3aab68a16602bf3e20fb3178d6e
Nicholas Ly <nichdly@gmail.com>
2024-02-11 17:45:26 -0600

    Added helper types to renamed  file

---
+++ src/utils/types.ts
@@ -0,0 +1,28 @@
+import { type InferSelectModel } from "drizzle-orm";
+import {
+  type users,
+  type posts,
+  type replies,
+  type reposts,
+  type likes,
+  type follows
+} from "~/server/db/schema";
+
+//
+// Utility Types
+//
+
+export type NotUndefined<T> = T extends undefined ? never : T;
+export type NotNull<T> = T extends null ? never : T;
+export type NotNullish<T> = T extends (null | undefined) ? never : T;
+
+//
+// Database Entity Types
+//
+
+export type User = InferSelectModel<typeof users>;
+export type Post = InferSelectModel<typeof posts>;
+export type Reply = InferSelectModel<typeof replies>;
+export type Repost = InferSelectModel<typeof reposts>;
+export type Like = InferSelectModel<typeof likes>;
+export type Follow = InferSelectModel<typeof follows>;
