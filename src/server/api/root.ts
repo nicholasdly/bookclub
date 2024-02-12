c37c33bca1c3f5aeba966c7261fd90d24e4b8ba6
Nicholas Ly <nichdly@gmail.com>
2024-02-11 18:44:59 -0600

    Added missing router definitions to

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,6 +1,9 @@
 import { postRouter } from "~/server/api/routers/post";
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
 import { userRouter } from "./routers/user";
+import { feedRouter } from "./routers/feed";
+import { replyRouter } from "./routers/reply";
+import { repostRouter } from "./routers/repost";
 
 /**
  * This is the primary router for your server.
@@ -8,8 +11,11 @@ import { userRouter } from "./routers/user";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  posts: postRouter,
   users: userRouter,
+  feed: feedRouter,
+  posts: postRouter,
+  replies: replyRouter,
+  reposts: repostRouter,
 });
 
 // export type definition of API
