3e460a021278de26759f7987c68048f57acc96a9
Nicholas Ly <nichdly@gmail.com>
2024-09-13 11:10:07 -0500

    feat: added shadcn/ui, drizzle, and auth.js

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,10 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  webpack: (config) => {
+    config.externals.push("@node-rs/argon2");
+    return config;
+  },
 };
 
 export default nextConfig;
