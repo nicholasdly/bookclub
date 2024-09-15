9148a88f9829f921f47d6d66825c4da62e411559
Nicholas Ly <nichdly@gmail.com>
2024-09-15 02:51:25 -0500

    feat: post feed loading indicator

--- src/components/feed.tsx
+++ src/components/feed.tsx
@@ -2,12 +2,22 @@
 
 import { api } from "@/trpc/react";
 import Post from "./post";
+import { LoaderCircleIcon } from "lucide-react";
+
+function Loading() {
+  return (
+    <div className="mt-8 flex justify-center">
+      <LoaderCircleIcon className="size-5 animate-spin" />
+    </div>
+  );
+}
 
 export default function Feed() {
-  const { data: posts } = api.post.getAll.useQuery();
+  const { data: posts, isLoading } = api.post.getAll.useQuery();
 
   return (
     <div className="divide-y">
+      {isLoading && <Loading />}
       {posts?.map((post) => <Post key={post.id} {...post} />)}
     </div>
   );
