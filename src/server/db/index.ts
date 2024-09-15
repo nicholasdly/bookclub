3233479e756e11b8b70fa216c5e55cde31a6914c
Nicholas Ly <nichdly@gmail.com>
2024-09-15 01:58:18 -0500

    feat: development database connection caching

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,9 +1,20 @@
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
+import { env } from "@/env";
 
 import * as schema from "./schema";
 
-const sql = neon(process.env.DATABASE_URL!);
+/**
+ * Cache the database connection in development. This avoids creating a new
+ * connection on every HMR update.
+ */
+const globalForDb = globalThis as unknown as {
+  sql: ReturnType<typeof neon>;
+};
+
+const sql = globalForDb.sql ?? neon(env.DATABASE_URL);
+if (env.NODE_ENV !== "production") globalForDb.sql = sql;
+
 const db = drizzle(sql, { schema });
 
 export default db;
