ceb20a7b1c854c2183cfee6cc46ff4683a7fd836
Nicholas Ly <hello@nicholasly.com>
2024-10-21 00:09:43 -0500

    refactor: user button

--- src/components/user-button.tsx
+++ src/components/user-button.tsx
@@ -21,7 +21,7 @@ import { Button } from "./ui/button";
 import { Separator } from "./ui/separator";
 import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
 
-export default function UserButton({ user }: { user: User }) {
+function UserMenu({ user }: { user: User }) {
   const [isPending, startTransition] = useTransition();
 
   const signout = () => {
@@ -62,6 +62,74 @@ export default function UserButton({ user }: { user: User }) {
   );
 
   return (
+    <div className="flex h-full w-full flex-col">
+      <section className="mb-2 flex items-center gap-2">
+        <div className="relative aspect-square h-full overflow-hidden rounded-full border">
+          <Image
+            src={user.user_metadata.avatar_url}
+            alt={user.user_metadata.username}
+            fill
+          />
+        </div>
+        <p className="flex flex-col">
+          <span className="text-nowrap text-sm font-semibold">
+            {user.user_metadata.username}
+          </span>
+          <span className="text-nowrap text-sm text-muted-foreground">
+            {user.user_metadata.display_name}
+          </span>
+        </p>
+      </section>
+      <Separator className="my-2" />
+      <nav className="flex flex-col">
+        <Navlink url="/home" label="Your Profile" icon={<UserIcon />} />
+        <Navlink url="/home" label="Your Books" icon={<BookMarkedIcon />} />
+        <Navlink url="/home" label="Your Clubs" icon={<UsersIcon />} />
+        <Separator className="my-2" />
+        <Navlink url="/home" label="Settings" icon={<SettingsIcon />} />
+        <Separator className="my-2" />
+        <Navlink url="/home" label="Bookclub Docs" icon={<BookOpenIcon />} />
+        <Navlink
+          url="/home"
+          label="Bookclub Roadmap"
+          icon={<ConstructionIcon />}
+        />
+        <Navlink url="/home" label="Bookclub Support" icon={<UsersIcon />} />
+      </nav>
+      <Separator className="my-2" />
+      <Button
+        variant="ghost"
+        className="h-fit justify-start p-1.5 hover:bg-neutral-200"
+        disabled={isPending}
+        onClick={() => signout()}
+      >
+        <LogOutIcon /> Sign out
+      </Button>
+      <section className="mt-auto">
+        <p className="text-xs font-medium text-muted-foreground">
+          &copy; {new Date().getFullYear()} Nicholas Ly
+        </p>
+        <div className="mb-1.5 mt-3 flex flex-col">
+          <Sublink url="/" label="Terms and Conditions" />
+          <Sublink url="/" label="Privacy Policy" />
+          <Sublink url="/" label="Code of Conduct" />
+        </div>
+        <Button
+          variant="link"
+          className="h-fit p-0 text-xs text-muted-foreground underline-offset-1"
+        >
+          Cookie settings
+        </Button>
+        <p className="text-xs font-medium">
+          We do not sell or share your personal information.
+        </p>
+      </section>
+    </div>
+  );
+}
+
+export default function UserButton({ user }: { user: User }) {
+  return (
     <Sheet>
       <SheetTrigger asChild>
         <Button
@@ -77,77 +145,7 @@ export default function UserButton({ user }: { user: User }) {
         </Button>
       </SheetTrigger>
       <SheetContent className="w-80 rounded-md border-l bg-sidebar p-4 text-sidebar-foreground">
-        <div className="flex h-full w-full flex-col">
-          <section className="mb-2 flex items-center gap-2">
-            <div className="relative aspect-square h-full overflow-hidden rounded-full border">
-              <Image
-                src={user.user_metadata.avatar_url}
-                alt={user.user_metadata.username}
-                fill
-              />
-            </div>
-            <p className="flex flex-col">
-              <span className="text-nowrap text-sm font-semibold">
-                {user.user_metadata.username}
-              </span>
-              <span className="text-nowrap text-sm text-muted-foreground">
-                {user.user_metadata.display_name}
-              </span>
-            </p>
-          </section>
-          <Separator className="my-2" />
-          <nav className="flex flex-col">
-            <Navlink url="/home" label="Your Profile" icon={<UserIcon />} />
-            <Navlink url="/home" label="Your Books" icon={<BookMarkedIcon />} />
-            <Navlink url="/home" label="Your Clubs" icon={<UsersIcon />} />
-            <Separator className="my-2" />
-            <Navlink url="/home" label="Settings" icon={<SettingsIcon />} />
-            <Separator className="my-2" />
-            <Navlink
-              url="/home"
-              label="Bookclub Docs"
-              icon={<BookOpenIcon />}
-            />
-            <Navlink
-              url="/home"
-              label="Bookclub Roadmap"
-              icon={<ConstructionIcon />}
-            />
-            <Navlink
-              url="/home"
-              label="Bookclub Support"
-              icon={<UsersIcon />}
-            />
-          </nav>
-          <Separator className="my-2" />
-          <Button
-            variant="ghost"
-            className="h-fit justify-start p-1.5 hover:bg-neutral-200"
-            disabled={isPending}
-            onClick={() => signout()}
-          >
-            <LogOutIcon /> Sign out
-          </Button>
-          <section className="mt-auto">
-            <p className="text-xs font-medium text-muted-foreground">
-              &copy; {new Date().getFullYear()} Nicholas Ly
-            </p>
-            <div className="mb-1.5 mt-3 flex flex-col">
-              <Sublink url="/" label="Terms and Conditions" />
-              <Sublink url="/" label="Privacy Policy" />
-              <Sublink url="/" label="Code of Conduct" />
-            </div>
-            <Button
-              variant="link"
-              className="h-fit p-0 text-xs text-muted-foreground underline-offset-1"
-            >
-              Cookie settings
-            </Button>
-            <p className="text-xs font-medium">
-              We do not sell or share your personal information.
-            </p>
-          </section>
-        </div>
+        <UserMenu user={user} />
       </SheetContent>
     </Sheet>
   );
